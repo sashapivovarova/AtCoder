506160246284f95f7bf02c07656701e658fa0abd
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-01-13 21:47:48 +0900

    Welcome to AtCoder

--- AtCoder/main.swift
+++ AtCoder/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+func main() {
+    let a: Int = 1
+    let b: Int = 2
+    let c: Int = 3
+    let s: String = " test"
+    
+    print(String(a+b+c)+s)
+}
 
+main()
